d57512d04f39958bbdd75f7116d75314c224b3a3
Cherukuri <P2891954@CORP.CHARTERCOM.COM>
2019-12-26 14:24:27 -0600

    sample program on methods and classes

--- src/com/sb/practice/Building.java
+++ src/com/sb/practice/Building.java
@@ -2,6 +2,29 @@ package com.sb.practice;
 
 public class Building {
 	// Attributes
-	
+	public int sqfland;
+	public String owner;
+	public int nooffloors;
 	// Activities
+	public boolean floor_paln() {
+		System.out.println("floor paln is ready");
+		return true;
+		}
+	public void sqf() {
+		System.out.println("noof sqfoot  "+ sqfland);
+		}
+	public void own() {
+		System.out.println("building owned by"+ " " + owner);
+		}
+	public void floors() {
+		System.out.println("how many floors " + nooffloors);
+		}
+	public void floorplan() {
+		if (nooffloors == 2 ) {
+			System.out.println("floor plan is to build 2 floors duplex with car parking " );
+		}
+		else {
+			System.out.println(" 2 floors are duplex, 2 floors independent. car and 4 bikes parking");
+		}
+		}
 }
--- src/com/sb/practice/Tester.java
+++ src/com/sb/practice/Tester.java
@@ -6,6 +6,24 @@ public class Tester {
 		Building building2 = new Building();
 		
 		// building 1 is owned by Sarath. It's in 150 sq.yds. 2 floors duplex with car parking.
+		building.sqfland = 150;
+		building.owner = "sarath";
+		building.nooffloors = 2;
+		building.floor_paln();
+		building.sqf();
+		building.floors();
+		building.floorplan();
+		building.own();
 		// building 2 is owned by Chaitanya. It's in 250 sq.yds. 4 floors - 2 floors are duplex, 2 floors independent. car, 4 bikes parking.
+		building2.sqfland = 250;
+		building2.owner = "chaitanya";
+		building2.nooffloors = 4;
+		building2.floor_paln();
+		building2.sqf();
+		building2.floors();
+		building2.floorplan();
+		building2.own();
+
+
 	}
 }
