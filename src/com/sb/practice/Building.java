da2572fdbca2d992d656b356c917dc6390312530
sarathcakurathi <sarath.c.akurathi@gmail.com>
2019-12-27 07:43:04 +0530

    Practice: Building floor and parking capacity

--- src/com/sb/practice/Building.java
+++ src/com/sb/practice/Building.java
@@ -2,29 +2,39 @@ package com.sb.practice;
 
 public class Building {
 	// Attributes
-	public int sqfland;
+	public int area;
 	public String owner;
-	public int nooffloors;
+	public int noOfFloors;
+	
+	public boolean isCarParkingAvailable;
+	public int carParkingCapacity;
+	
+	public boolean isBikeParkingAvailable;
+	public int bikeParkingCapacity;
+
 	// Activities
-	public boolean floor_paln() {
+	public boolean isFloorPlanReady() {
 		System.out.println("floor paln is ready");
 		return true;
-		}
-	public void sqf() {
-		System.out.println("noof sqfoot  "+ sqfland);
-		}
-	public void own() {
-		System.out.println("building owned by"+ " " + owner);
-		}
-	public void floors() {
-		System.out.println("how many floors " + nooffloors);
-		}
-	public void floorplan() {
-		if (nooffloors == 2 ) {
-			System.out.println("floor plan is to build 2 floors duplex with car parking " );
-		}
-		else {
+	}
+
+	public void getArea() {
+		System.out.println("noof sqfoot  " + area);
+	}
+
+	public void getOwner() {
+		System.out.println("building owned by " + owner);
+	}
+
+	public void getNoOfFloors() {
+		System.out.println("how many floors " + noOfFloors);
+	}
+
+	public void floorPlan() {
+		if (noOfFloors == 2) {
+			System.out.println("floor plan is to build 2 floors duplex with car parking ");
+		} else {
 			System.out.println(" 2 floors are duplex, 2 floors independent. car and 4 bikes parking");
 		}
-		}
+	}
 }
--- src/com/sb/practice/Tester.java
+++ src/com/sb/practice/Tester.java
@@ -6,23 +6,31 @@ public class Tester {
 		Building building2 = new Building();
 		
 		// building 1 is owned by Sarath. It's in 150 sq.yds. 2 floors duplex with car parking.
-		building.sqfland = 150;
+		building.area = 150;
 		building.owner = "sarath";
-		building.nooffloors = 2;
-		building.floor_paln();
-		building.sqf();
-		building.floors();
-		building.floorplan();
-		building.own();
+		building.noOfFloors = 2;
+		building.isCarParkingAvailable = true;
+		building.isBikeParkingAvailable = false;
+		
+		// ToDo: Chaitanya
+		//building.setCarParkingCapacity(1);
+		//building.setBikeParkingCapacity(2);
+		
+		building.isFloorPlanReady();
+		building.getArea();
+		building.getNoOfFloors();
+		building.floorPlan();
+		building.getOwner();
+		
 		// building 2 is owned by Chaitanya. It's in 250 sq.yds. 4 floors - 2 floors are duplex, 2 floors independent. car, 4 bikes parking.
-		building2.sqfland = 250;
+		building2.area = 250;
 		building2.owner = "chaitanya";
-		building2.nooffloors = 4;
-		building2.floor_paln();
-		building2.sqf();
-		building2.floors();
-		building2.floorplan();
-		building2.own();
+		building2.noOfFloors = 4;
+		building2.isFloorPlanReady();
+		building2.getArea();
+		building2.getNoOfFloors();
+		building2.floorPlan();
+		building2.getOwner();
 
 
 	}
